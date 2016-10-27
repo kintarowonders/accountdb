b85d27739a66f0b0b1114061408c3021ae3f5657
John Tate <john@johntate.org>
2016-10-28 04:47:05 +1100

    added some methods to the interface

--- accountdb.py
+++ accountdb.py
@@ -55,6 +55,128 @@ def addUser(user):
 def addUserContact(user):
     ctype = input('Contact type: ')
     detail = input('Contact detail: ')
-    database.addContact(user, ctype, detail)
 
+    if (not ctype):
+        print("You must specify a contact type.")
+        return
+    if (not detail):
+        print("You must specify a contact detail.")
+        return
+
+    database.addContact(user, ctype, detail)
+    print("Contact detail added to database.")
+
+def renameUser(user):
+    print("Renaming user.")
+    newname = input('New username: "')
     
+    if (not newname):
+        print("You must specify a new username.")
+        return
+
+    changeUser(user, 0, "", newname)
+
+def searchContact():
+    ctype = input('Contact type: ')
+    detail = input('Contact detail: ')
+    
+    uid = database.searchContact(ctype, detail)
+    user = database.getContactById(uid)
+    
+    if (uid == -1):
+        print("No such contact")
+    else:
+        print("Contact belongs to " + user + ".")
+        showUser(user)
+
+def modifyContact(user):
+    print("Modifying contact detail.")
+    showUser(user)
+    ctype = input('Contact type to change: ')
+    detail = input('New contact detail: ')
+    if (database.changeContact(user, ctype, detail, "")):
+        print("User modified")
+    else
+        print("User modification failed")
+
+if (argv.length == 1):
+    print("You didn't specify any command line arguments.")
+    sys.exit()
+
+def modifyUserDue(user):
+    print("Modifying user due date.")
+    newdate = input("Specify a date YYYY-MM-DD or an duration, eg: D1, W1, M1: ")
+
+    if (not newdate):
+        print ("You must specify a date or duration")
+        return
+
+    due = database.getUserDue(user)
+
+    x = 0
+    days = False
+    weeks = False
+    months = False
+    doDuration = False
+    durationString = ""
+    for c in newdate:
+        if (x == 0):
+            if (newdate[0] == "D"):
+                days = True
+                doDuration = True
+            if (newdate[0] == "W"):
+                weeks = True
+                doDuration = True
+            if (newdate[0] == "M"):
+                months = True
+                doDuration = True
+            if (x >= 1):
+                if (doDuration):
+                    durationString = durationString + c
+   if (doDuration):
+        durationInt = int(durationString)
+        if (days):
+            seconds = durationInt * 24 * 60 * 60
+        if (weeks)
+            seconds = durationInt * 7 * 24 * 60 * 60
+        if (months)
+            seconds = durationInt * 31 * 24 * 60 * 60
+        due = due + seconds
+    else:
+        year, month, day = map(int, date_entry.split('-'))
+        due = datetime.date(year, month, day)
+
+    database.changeUser(user, due, "", "")   
+    print("Database updated!")
+
+def changeUserType(user):
+    print("Changing customer type.")
+    newtype = input("New type: ")
+    
+    if (not newtype):
+        print("You must specify a new type.")
+        return
+
+    database.changeUser(user, 0, newtype, "")
+
+#argument handler
+success = False
+if (argv[1] == "show"):
+    if (argv.length != 4):
+        print("Invalid number of arguments.")
+        sys.exit()
+    if (argv[2] == "user"):
+        showUser(argv[3])
+        success = True
+    if (not success):
+        print("Invalid arguments")
+        sys.exit()
+    sys.exit()
+if (argv[1] == "edit"):
+   
+if (argv[1] == "add"):
+
+if (argv[1] == "remove"):
+
+if (argv[1] == "bill"):
+    showDueBills()
